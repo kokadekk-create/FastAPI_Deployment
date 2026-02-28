e8ad317830f97da99b68e6f666abf3f359ff509c
kokadekk-create <kokadekk@gmail.com>
2026-02-28 09:25:23 +0530

    add 2 server

--- frontend/app.py
+++ frontend/app.py
@@ -1,2 +1,17 @@
 import streamlit as st
 import requests
+API_BASE = "http://localhost:8000"
+
+st.title("Welcome to Mastery on AI")
+st.subheader("Live Hands-on training! ")
+st.title("Calculator UI")
+
+a = st.number_input("Enter first number")
+b = st.number_input("Enter second number")
+if st.button("Add"):
+    response = requests.get(
+        "http://localhost:8000/add",
+        params={"a": a, "b": b}
+    )
+    #st.write("Result:", response.json())
+    st.write("Result:", response.json()["result"])
