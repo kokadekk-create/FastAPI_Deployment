1c32462ee794790407e1b17ea0f0710bd294a5b9
kokadekk-create <kokadekk@gmail.com>
2026-02-28 08:24:07 +0530

    Updated main.py

--- backend/main.py
+++ backend/main.py
@@ -2,3 +2,23 @@ from fastapi import FastAPI
 import requests
 import os
 from dotenv import load_dotenv
+
+load_dotenv()
+
+
+#OPENROUTER_API_KEY = os.environ.get("OPENROUTER_API_KEY")
+
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "FastAPI backend running"}
+
+@app.get("/add")
+def add(a: int, b: int):
+    return {"result": a + b}
+
+@app.get("/multiply")
+def multiply(a: int, b: int):
+    return {"result": a * b}
